a4865b62bbe416745069d7624724ba795e2bdddd
Chris Kavanagh <ckava3@gmail.com>
2021-06-13 16:57:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [flatLocations, setFlatLocations] = useState([]);
+  const h3Class = {
+    marginLeft: "1rem",
+    color: "#808000",
+  };
+
+  const h2Class = {
+    color: "#CD5C5C",
+    fontWeight: "bolder",
+    marginLeft: "1rem",
+  };
+
+  const h4Class = {
+    color: "#191970",
+    fontWeight: "bold",
+    marginLeft: "1rem",
+  };
+
+  const longitudeClass = {
+    display: "inline-block",
+    marginLeft: "15px",
+  };
+
+  const latitudeClass = {
+    display: "inline-block",
+    marginLeft: "1rem",
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("https://randomuser.me/api/?results=20");
+      setPeople(res.data.results);
+    };
+    fetchData();
+  }, []);
+
+  function flatten(objects) {
+    return objects.map(({ location }) => location);
+  }
+
+  useEffect(() => {
+    setFlatLocations(flatten(people));
+  }, [people]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactJS Coding Interview</h1>
+      {console.log(flatLocations)}
+      {flatLocations.map((p, i) => (
+        <div key={i}>
+          <h2 style={h2Class}>{p.country}</h2>
+          <h3 style={h3Class}>{p.state}</h3>
+          <h4 style={h4Class}>{p.city}</h4>
+          <h4 style={latitudeClass}>Lat: {p.coordinates.latitude}</h4>
+          <h4 style={longitudeClass}>Long: {p.coordinates.longitude}</h4>
+          <hr />
+        </div>
+      ))}
+      <hr />
     </div>
   );
 }
