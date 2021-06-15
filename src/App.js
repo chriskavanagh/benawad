4c98d102deaca5a504e0a5bc3e91ffa754dca472
Chris Kavanagh <ckava3@gmail.com>
2021-06-15 02:41:37 -0400

    table

--- src/App.js
+++ src/App.js
@@ -5,37 +5,12 @@ import "./App.css";
 function App() {
   const [people, setPeople] = useState([]);
   const [flatLocations, setFlatLocations] = useState([]);
-  const h3Class = {
-    marginLeft: "1rem",
-    color: "#808000",
-  };
-
-  const h2Class = {
-    color: "#CD5C5C",
-    fontWeight: "bolder",
-    marginLeft: "1rem",
-  };
-
-  const h4Class = {
-    color: "#191970",
-    fontWeight: "bold",
-    marginLeft: "1rem",
-  };
-
-  const longitudeClass = {
-    display: "inline-block",
-    marginLeft: "15px",
-  };
-
-  const latitudeClass = {
-    display: "inline-block",
-    marginLeft: "1rem",
-  };
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get("https://randomuser.me/api/?results=20");
       setPeople(res.data.results);
+      console.log(res.data.results);
     };
     fetchData();
   }, []);
@@ -49,22 +24,68 @@ function App() {
   }, [people]);
 
   return (
-    <div>
-      <h1>ReactJS Coding Interview</h1>
+    <div style={wrapperClass}>
+      <h1 style={h1Class}>ReactJS Coding Interview</h1>
       {console.log(flatLocations)}
-      {flatLocations.map((p, i) => (
-        <div key={i}>
-          <h2 style={h2Class}>{p.country}</h2>
-          <h3 style={h3Class}>{p.state}</h3>
-          <h4 style={h4Class}>{p.city}</h4>
-          <h4 style={latitudeClass}>Lat: {p.coordinates.latitude}</h4>
-          <h4 style={longitudeClass}>Long: {p.coordinates.longitude}</h4>
-          <hr />
-        </div>
-      ))}
-      <hr />
+      <table>
+        <tr>
+          <th style={headerClass}>City</th>
+          <th style={headerClass}>State</th>
+          <th style={headerClass}>Country</th>
+          <th style={headerClass}>Lat</th>
+          <th style={headerClass}>Long</th>
+        </tr>
+        {flatLocations.map((p, i) => (
+          <tr key={i}>
+            <td style={h2Class}>{p.country}</td>
+            <td style={h3Class}>{p.state}</td>
+            <td style={h4Class}>{p.city}</td>
+            <td style={latClass}>{p.coordinates.latitude}</td>
+            <td>{p.coordinates.longitude}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
 
 export default App;
+
+const wrapperClass = {
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+};
+
+const h1Class = {
+  marginBottom: "5rem",
+  marginTop: "1rem",
+};
+
+const h3Class = {
+  marginLeft: "1rem",
+  color: "#808000",
+};
+
+const h2Class = {
+  color: "#CD5C5C",
+  fontWeight: "bolder",
+  marginLeft: "1rem",
+};
+
+const h4Class = {
+  color: "#191970",
+  fontWeight: "bold",
+  marginLeft: "1rem",
+};
+
+const latClass = {
+  paddingLeft: "2rem",
+  paddingRight: "3rem",
+};
+
+const headerClass = {
+  fontSize: "21px",
+  paddingBottom: "1.5rem",
+  textDecoration: "underline",
+};
